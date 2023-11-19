39fd02c17fa522212293ee33de8916c396014606
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2023-11-19 20:48:45 +0700

    autoload php

---
+++ chapter5/classes/cat.php
@@ -0,0 +1,11 @@
+<?php
+
+class Cat
+{
+  public $name = "Meowww";
+
+  public function sayHello()
+  {
+    return "Hi, Meowww!";
+  }
+}
---
+++ chapter5/classes/student.php
@@ -0,0 +1,11 @@
+<?php
+
+class Student
+{
+  public $name = "Lolo";
+
+  public function sayHello()
+  {
+    return "Hi, Lolo!";
+  }
+}
